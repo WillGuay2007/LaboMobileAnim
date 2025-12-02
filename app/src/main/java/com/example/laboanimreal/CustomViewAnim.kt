44ad382a2d9e12cbe936ab282d8f402b26d47fdd
William Guay <WillGuay2007@hotmail.com>
2025-12-02 00:35:05 -0500

    e

--- app/src/main/java/com/example/laboanimreal/CustomViewAnim.kt
+++ app/src/main/java/com/example/laboanimreal/CustomViewAnim.kt
@@ -21,6 +21,13 @@ class CustomViewAnim @JvmOverloads constructor(
     var cx = 0f
     var cy = 0f
     var radius = 1f
+    var time = 0f
+        get() = field
+        set(value) {
+            field = value
+            invalidate()
+        }
+
 
     init {
         myPaint.color = Color.BLUE
@@ -29,13 +36,16 @@ class CustomViewAnim @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawCircle(cx,cy,radius,myPaint)
-        drawTriangle(canvas, 3f)
+        canvas.translate(cx,cy)
+        canvas.rotate(-90f)
+        canvas.rotate(time)
+        drawPolygon(canvas, radius/3, 4)
+
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         Log.d("MainActivity", "Touched")
-        invalidate()
+        time += 1
         return super.onTouchEvent(event)
     }
 
@@ -45,25 +55,6 @@ class CustomViewAnim @JvmOverloads constructor(
         cy = h * 0.5f
         radius = min(cx,cy)
     }
-
-    private fun drawTriangle(canvas: Canvas, size:Float) {
-
-
-
-        repeat(3) {
-
-            canvas.save()
-            canvas.translate(size, 0f)
-            canvas.rotate(180f - 30)
-            canvas.drawLine(0f, 0f, size, 0f, myPaint)
-            canvas.rotate(60f)
-            canvas.drawLine(0f, 0f, size, 0f, myPaint)
-            canvas.restore()
-
-            canvas.rotate(120f)
-        }
-    }
-
 
     private fun drawPolygon(canvas: Canvas, size:Float, sides:Int) {
 
@@ -78,6 +69,10 @@ class CustomViewAnim @JvmOverloads constructor(
             canvas.drawLine(0f, 0f, size, 0f, myPaint)
             canvas.rotate(cornerAngle)
             canvas.drawLine(0f, 0f, size, 0f, myPaint)
+            if (sides > 3) {
+                canvas.rotate(time)
+                drawPolygon(canvas, size / 2, sides - 1)
+            }
             canvas.restore()
 
             canvas.rotate(centralAngle)
